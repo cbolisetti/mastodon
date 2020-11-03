8228860b7a82e4a3054b03e4da61df94f1ec289d
Chandu Bolisetti <chandu774@gmail.com>
2020-11-03 15:43:47 -0700

    Minor fix. Refs #3.

--- python/mastodonutils/__init__.py
+++ python/mastodonutils/__init__.py
@@ -1,5 +1,5 @@
 """General utilities for MASTODON"""
 import sys
-if not (sys.version_info[0]=='2'):
+if not (sys.version_info[0]==2):
     ver = sys.version_info[0]
     raise Exception ('Failed to execute. The MASTODON Python module currently runs on Python2. You are currently using Python{}.'.format(ver))
